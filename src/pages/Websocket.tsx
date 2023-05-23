53345a8f3a381902a205c402125edaec9373f5e5
Alastair Scheuermann <alastairscheuermann@gmail.com>
2023-05-23 15:09:58 -0600

    socket progress
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>
    Co-authored-by: Justin Dilts <Suigin1@users.noreply.github.com>
    Co-authored-by: Luke Lauther <lukelauther@users.noreply.github.com>
    Co-authored-by: Stephen Havig <Stephen-Havig@users.noreply.github.com>

--- src/pages/Websocket.tsx
+++ src/pages/Websocket.tsx
@@ -3,9 +3,41 @@ import React, { useState, useEffect } from 'react';
 
 function WebSocket() {
   return (
-    <div>
-      <h1>Websocket</h1>
-    </div>
+    <body
+      style={{
+        margin: '0 auto',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100vh',
+        width: '90%',
+        maxWidth: '1200px',
+      }}
+    >
+      <div
+        id="message-container"
+        style={{
+          border: '1px solid black',
+          flexGrow: 1,
+          overflowY: 'auto',
+          backgroundColor: '#CCC',
+          padding: '5px',
+        }}
+      >
+        <form id="form">
+          <label htmlFor="message-input"></label>
+          <input type="text" id="message-input"></input>
+          <button type="submit" id="send-button">
+            Send
+          </button>
+          <label htmlFor="room-input">Room</label>
+          <input type="text" id="room-input"></input>
+          <button type="button" id="room-button">
+            Join
+          </button>
+        </form>
+      </div>
+    </body>
   );
 }
 
