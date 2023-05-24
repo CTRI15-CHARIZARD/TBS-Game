811d3249db3bf70059aed5b6ce8f157a5d94bd12
Suigin1 <justin.dilts@mail.utoronto.ca>
2023-05-24 13:20:37 -0400

    initial setup for backend completed - Co-authored-by: Luke Lauther <lukelauther@users.noreply.github.com> Co-authored-by: Alastair <alastairsounds@users.noreply.github.com> Co-authored-by: Stephen Havig <Stephen-Havig@users.noreply.github.com>

---
+++ server/controllers/moveController.ts
@@ -0,0 +1,18 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+
+const fetch = require('node-fetch');
+
+const moveController = {
+  getMoveData: async (req: Request, res: Response, next: NextFunction) => {
+    try { 
+      const moveId: (number | string) = req.params.id;
+      const moveData:JSON = res.locals.moveData = await fetch(`https://pokeapi.co/api/v2/move/${moveId}`).json()
+      return next();
+    }
+    catch (err: (any | undefined)) { 
+      return next({err: 'error retrieving move data'})
+    }
+  }
+};
+
+module.exports = moveController;
