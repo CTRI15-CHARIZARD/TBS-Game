9a5636c606257800073f7484b7054fda2796803c
Suigin1 <justin.dilts@mail.utoronto.ca>
2023-05-24 17:42:39 -0400

    adding data for Pokemon stats

---
+++ src/store/pokemonData.tsx
@@ -0,0 +1,45 @@
+const bulbasaur = { 
+  name: 'bulbasaur',
+  hp: 120,
+  moves: {
+    vine_whip: 20,
+    leech_seed: 17,
+    mega_drain: 30
+  }
+ };
+const chamander = {
+  name: 'charmander',
+  hp: 100,
+  moves: {
+    fire_punch: 20,
+    ember: 25,
+    flamethrower: 28,
+  }
+};
+const squirtle = {
+  name: 'squirtle',
+  hp: 130,
+  moves: {
+    headbutt: 20,
+    water_gun: 30,
+    hydro_pump: 35,
+  }
+};
+const pikachu = {
+  name: 'pikachu',
+  hp: 115,
+  moves: {
+    thunder: 35,
+    double_kick: 20,
+    headbutt: 15,
+  }
+};
+const jigglypuff = {
+  name: 'jigglypuff',
+  hp: 150,
+  moves: {
+    body_slam: 15,
+    sing: 30,
+    double_slap: 25,
+  }
+};
