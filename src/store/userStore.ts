44d3780139a14f8dadeafde43126c61562bda769
Stephen.Havig <stephen.havig@gmail.com>
2023-05-24 08:32:59 -0700

    completed merge with lukes battle screen

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,22 +1,22 @@
-import { createContext, useContext, useState } from 'react';
+// import { createContext, useContext, useState } from 'react';
 
-const StoreContext = createContext('');
+// const StoreContext = createContext('');
 
-export function StoreProvider({ children }) {
-  const [userData, setUserData] = useState(null);
+// export function StoreProvider({ children }) {
+//   const [userData, setUserData] = useState(null);
 
-  const setUser = (user) => {
-    setUserData(user);
-  };
+//   const setUser = (user) => {
+//     setUserData(user);
+//   };
 
-  const store = {
-    userData,
-    setUser,
-  };
+//   const store = {
+//     userData,
+//     setUser,
+//   };
 
-  return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>;
-}
+//   return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>;
+// }
 
-export function useStore() {
-  return useContext(StoreContext);
-}
+// export function useStore() {
+//   return useContext(StoreContext);
+// }
